f3a18f7f27377d3ef9e814381f31b4bd08052efd
nmvivas <nmvivas@espe.edu.ec>
2024-08-02 03:12:06 -0500

    [UPDATE] class generation unique Id

--- src/main/java/com/banquito/corecobros/companydoc/util/uniqueId/UniqueIdGeneration.java
+++ src/main/java/com/banquito/corecobros/companydoc/util/uniqueId/UniqueIdGeneration.java
@@ -5,55 +5,35 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 public class UniqueIdGeneration {
-	private String uniqueId;
 
-	public UniqueIdGeneration() {
-		this.uniqueId = generateUniqueId();
-	}
-
-	private String generateUniqueId() {
-		String letters = generateRandomLetters(3);
-		String numbers = generateRandomNumbers(5);
-		String rawId = letters + "00" + numbers;
-		return encryptWithMD5(rawId);
-	}
-
-	private String generateRandomLetters(int length) {
-		Random random = new Random();
-		StringBuilder sb = new StringBuilder(length);
-		for (int i = 0; i < length; i++) {
-			char letter = (char) ('A' + random.nextInt(26));
-			sb.append(letter);
+		public String generateUniqueId() {
+			String letters = generateRandomLetters(3);
+			String numbers = generateRandomNumbers(5);
+			String rawId = letters + "00" + numbers;
+			return rawId;
 		}
-		return sb.toString();
-	}
-
-	private String generateRandomNumbers(int length) {
-		Random random = new Random();
-		StringBuilder sb = new StringBuilder(length);
-		for (int i = 0; i < length; i++) {
-			int number = random.nextInt(10);
-			sb.append(number);
-		}
-		return sb.toString();
-	}
-
-	private String encryptWithMD5(String input) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.update(input.getBytes());
-			byte[] digest = md.digest();
-			StringBuilder sb = new StringBuilder();
-			for (byte b : digest) {
-				sb.append(String.format("%02x", b));
+	
+		private String generateRandomLetters(int length) {
+			Random random = new Random();
+			StringBuilder sb = new StringBuilder(length);
+			for (int i = 0; i < length; i++) {
+				char letter = (char) ('A' + random.nextInt(26));
+				sb.append(letter);
 			}
 			return sb.toString();
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException("MD5 algorithm not found", e);
+		}
+	
+		private String generateRandomNumbers(int length) {
+			Random random = new Random();
+			StringBuilder sb = new StringBuilder(length);
+			for (int i = 0; i < length; i++) {
+				int number = random.nextInt(10);
+				sb.append(number);
+			}
+			return sb.toString();
+		}
+	
+		public String getUniqueId() {
+			return generateUniqueId();
 		}
 	}
-
-	public String getUniqueId() {
-		return uniqueId;
-	}
-}
